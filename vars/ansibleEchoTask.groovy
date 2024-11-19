8ac381ca23f54b733431646abf3aa714a044a4c4
Makara <148768781+Makara1122@users.noreply.github.com>
2024-11-19 13:14:12 +0700

    Update ansibleEchoTask.groovy

--- vars/ansibleEchoTask.groovy
+++ vars/ansibleEchoTask.groovy
@@ -1,9 +1,14 @@
-// ansibleEchoTask.groovy
 def call() {
-    // Run Ansible Playbook using the echo_task.yml playbook
+    // Fetch playbook from resources and save to workspace
+    writeFile file: 'ansible/echo_task.yml', text: libraryResource('ansible/echo_task.yml')
+    
+    // Fetch inventory from resources and save to workspace
+    writeFile file: 'ansible/inventory.ini', text: libraryResource('ansible/inventory.ini')
+    
+    // Run the Ansible Playbook
     ansiblePlaybook(
-        playbook: 'ansible/echo_task.yml',    // Path to the playbook within the library
-        inventory: 'ansible/inventory.ini',  // Path to the inventory file
+        playbook: 'ansible/echo_task.yml',    // Path in the workspace
+        inventory: 'ansible/inventory.ini',  // Path in the workspace
         ansibleOptions: '-v'                 // Verbose output for debugging
     )
 }
